d3399d10890522eba4126259beb7c8082708b424
Berna30 <bernaomb@outlook.com>
2026-04-11 21:33:34 -0400

    rajout package

--- Reservation/src/Model/Passager.java
+++ Reservation/src/Model/Passager.java
@@ -20,28 +20,4 @@ public class Passager
                 ", bagages=" + bagages +
                 '}';
     }
-
-    public String getNumPasseport() {
-        return numPasseport;
-    }
-
-    public void setNumPasseport(String numPasseport) {
-        this.numPasseport = numPasseport;
-    }
-
-    public String getNomComplet() {
-        return nomComplet;
-    }
-
-    public void setNomComplet(String nomComplet) {
-        this.nomComplet = nomComplet;
-    }
-
-    public double getBagages() {
-        return bagages;
-    }
-
-    public void setBagages(double bagages) {
-        this.bagages = bagages;
-    }
 }
